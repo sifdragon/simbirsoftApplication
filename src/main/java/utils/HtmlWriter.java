85240dea9b26ec28bec36f2782f2d544f390a99e
sifdragon <wistfuldr@mail.ru>
2021-04-08 22:25:09 +0300

    Update HtmlWriter.java
    #Cant access ide right now#
    Deleting forgotten printStrackTraces

--- src/main/java/utils/HtmlWriter.java
+++ src/main/java/utils/HtmlWriter.java
@@ -24,7 +24,6 @@ public class HtmlWriter {
                 outputStream.close();
             } catch (IOException e) {
                 logger.error("IOException happened!", e);
-                e.printStackTrace();
             }
         }
     }
